b5ecc4cc2617cf963534c306f44a13d2fba55bd2
Amolur <latenable@gmail.com>
2025-06-02 22:56:29 +0300

    Upd

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Content-Type', 'application/json');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
         return res.status(200).json({});
